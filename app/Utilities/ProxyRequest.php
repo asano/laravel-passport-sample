94928c266b6477fcada9b4c10c402b860222fc2e
asano <y.asano.0123@gmail.com>
2020-05-20 20:01:17 +0900

    API用AuthController設置

---
+++ app/Utilities/ProxyRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Utilities;
+
+class ProxyRequest
+{
+    // grant typeを"password"に設定
+    public function grantPasswordToken(string $email, string $password)
+    {
+        $params = [
+            'grant_type' => 'password',
+            'username' => $email,
+            'password' => $password,
+        ];
+
+        return $this->makePostRequest($params);
+    }
+
+    // アクセストークンの更新
+    public function refreshAccessToken()
+    {
+        $refreshToken = request()->cookie('refresh_token');
+
+        abort_unless($refreshToken, 403, 'Your refresh token is expired.');
+
+        $params = [
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $refreshToken,
+        ];
+
+        return $this->makePostRequest($params);
+    }
+
+    // 内部でPOST送信
+    protected function makePostRequest(array $params)
+    {
+        $params = array_merge([
+            'client_id' => config('services.passport.password_client_id'),
+            'client_secret' => config('services.passport.password_client_secret'),
+            'scope' => '*',
+        ], $params);
+
+        $proxy = \Request::create('oauth/token', 'post', $params);
+        $resp = json_decode(app()->handle($proxy)->getContent());
+
+        $this->setHttpOnlyCookie($resp->refresh_token);
+
+        return $resp;
+    }
+
+    // Cookie設定
+    protected function setHttpOnlyCookie(string $refreshToken)
+    {
+        cookie()->queue(
+            'refresh_token',
+            $refreshToken,
+            14400, // 10 days
+            null,
+            null,
+            false,
+            true // httponly
+        );
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('guest')->group(function () {
+    Route::post('register', 'Api\AuthController@register')->name('register');
+    Route::post('login', 'Api\AuthController@login')->name('login');
+    Route::post('refresh-token', 'Api\AuthController@refreshToken')->name('refreshToken');
+});
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', 'Api\AuthController@logout')->name('logout');
 });
